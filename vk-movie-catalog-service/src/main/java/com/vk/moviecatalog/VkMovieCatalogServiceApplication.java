60e54cbebf61cd778d8d8170946cb70e22026531
thanhkieuus <voltakieu@yahoo.com>
2019-09-07 13:35:22 -0500

    First Commit

---
+++ vk-movie-catalog-service/src/main/java/com/vk/moviecatalog/VkMovieCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vk.moviecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkMovieCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkMovieCatalogServiceApplication.class, args);
+	}
+
+}
